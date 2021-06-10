471d9d33f235d0bff592eaca884d38331e972647
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2021-06-10 11:03:36 +0800

    added context API with the cart item  updated

--- src/Components/context/cart/CartState.jsx
+++ src/Components/context/cart/CartState.jsx
@@ -3,27 +3,40 @@ import CartContext from './CartContext'
 import CartReducer from './CartReducer'
 import {SHOW_HIDE_CART, REMOVE_ITEM, ADD_TO_CART} from '../Types'
 
-const CartState = ({children}) => { 
-    const initialState = {  
-        showCart: false, 
-        cartItems: []
-    }
-    const [state, dispatch] = useReducer(CartReducer, initialState); 
-    const addToCart = item => {  
-        dispatch({type: ADD_TO_CART, payload: item})
-    }
-    const showHideCart = () => { 
-        dispatch({type: SHOW_HIDE_CART})
-    }
-    const removeItem = id => { 
-        dispatch({type: REMOVE_ITEM, payload:id})
-    }
-   return (
-    <CartContext.Provider value={{showCart: state.showCart, cartItems: state.cartItem, addToCart, showHideCart, removeItem}}>
-      {children}
-    </CartContext.Provider>
-  )
-}
-
-export default CartState
+const CartState = ({ children }) => {
+    const initalState = {
+      showCart: false,
+      cartItems: [],
+    };
+  
+    const [state, dispatch] = useReducer(CartReducer, initalState);
+  
+    const addToCart = (item) => {
+      dispatch({ type: ADD_TO_CART, payload: item });
+    };
+  
+    const showHideCart = () => {
+      dispatch({ type: SHOW_HIDE_CART });
+    };
+  
+    const removeItem = (id) => {
+      dispatch({ type: REMOVE_ITEM, payload: id });
+    };
+  
+    return (
+      <CartContext.Provider
+        value={{
+          showCart: state.showCart,
+          cartItems: state.cartItems,
+          addToCart,
+          showHideCart,
+          removeItem,
+        }}
+      >
+        {children}
+      </CartContext.Provider>
+    );
+  };
+  
+  export default CartState;
 
