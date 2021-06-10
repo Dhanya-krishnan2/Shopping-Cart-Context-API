1a374f2020fd7218d304920bb9ddb590e187ffbe
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2021-06-10 12:17:24 +0800

    added cart function with context

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import './Cart.css'
+import {useContext} from 'react'
+import './Cart.css';
+import CartContext from './context/cart/CartContext'
+import FormatCurrency from 'format-currency'
 
 const Cart = () => {
+    const {showCart,cartItems,showHideCart } = useContext(CartContext);
+    let opts = {format: "%s%v", Symbol: "$"};
     return (
-        <div>
-            
-        </div>
+        <>
+       { showCart && ( 
+           <div className="cart__wrapper"> 
+           <div style={{textAlign:"right"}}>
+               <i style={{cursor: 'pointer'}} className="fa fa-times"></i>
+           </div>
+           </div>
+       )}            
+        </>
     )
 }
 
