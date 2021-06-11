f12db78316b9594796aee2c11e6045a3faac7933
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2021-06-11 10:56:49 +0800

    added all the working flow of the cart functionality

--- src/Components/context/Types.jsx
+++ src/Components/context/Types.jsx
@@ -1,3 +1,4 @@
 export const SHOW_HIDE_CART = 'SHOW_HIDE_CART';
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_ITEM = 'REMOVE_ITEM';
+// In the types we are writing the types of action we are doing in the cart.
--- src/Components/context/cart/CartContext.jsx
+++ src/Components/context/cart/CartContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-
+// HERE WE ARE CREATING CONTEXT FROM REACT.
 const CartContext = createContext();
-
+// we are exporting the function 
 export default CartContext;
